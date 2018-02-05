29a272799c6435792c00e0411a8bd2e791cd055a
jrbrayjr2006 <jrbrayjr2006@gmail.com>
2018-02-04 20:10:42 -0600

    first commit

---
+++ src/main/java/com/jrbrayjr2018/amqp/simplerabbitmqspring/SimpleRabbitmqSpringApplication.java
@@ -0,0 +1,12 @@
+package com.jrbrayjr2018.amqp.simplerabbitmqspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRabbitmqSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRabbitmqSpringApplication.class, args);
+	}
+}
